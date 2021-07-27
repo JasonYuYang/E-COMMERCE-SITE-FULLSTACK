18ab12c1f3e788a1c4ac691f20c201d41eb5a603
Yu Yang <jasons0900107315@gmail.com>
2021-07-27 23:32:39 +0800

    Fixed Image

--- frontend/src/components/product/ProductCategory.js
+++ frontend/src/components/product/ProductCategory.js
@@ -8,7 +8,11 @@ const ProductCategory = ({ category, col }) => {
     <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
       <div className="card p-3 rounded">
         <Link to={`category/${category}`}>
-          <img className="card-img-top mx-auto" src={`/images/category/${categoryQuery}.png`} alt={categoryQuery} />
+          <img
+            className="card-img-top mx-auto"
+            src={`/public/images/category/${categoryQuery}.png`}
+            alt={categoryQuery}
+          />
         </Link>
         <div className="card-body d-flex flex-column">
           <h5 className="card-title card-text">
