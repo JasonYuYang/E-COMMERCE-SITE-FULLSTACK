661180b5c467bd88e0439ba2b2b09887aa36e7ec
Yu Yang <jasons0900107315@gmail.com>
2021-07-28 14:52:33 +0800

    fixed upload size

--- backend/app.js
+++ backend/app.js
@@ -46,8 +46,8 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
 
