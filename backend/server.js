27c68c6304867392df25065cbb63949fe6eb56ca
Yu Yang <jasons0900107315@gmail.com>
2021-07-30 14:37:19 +0800

    server.js add backup port

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = require('./app');
 
 connectDatabase();
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server Start on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 });
 
